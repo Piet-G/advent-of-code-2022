1546b333a4634b23785d6738acc729635d25697d
Piet Goris <piet.goris@gmail.com>
2022-12-08 18:20:45 +0100

    Day 7 Part 2

--- src/day7.rs
+++ src/day7.rs
@@ -27,6 +27,32 @@ impl Directory {
 
         return self.directories.iter().map(|dir| dir.borrow().get_sizes_under(max_size)).sum::<usize>() + own_size;
     }
+
+    fn flatten(directory: &Rc<RefCell<Directory>>) -> Vec<Rc<RefCell<Directory>>> {
+        let mut children: Vec<Rc<RefCell<Directory>>> = directory
+            .borrow()
+            .directories
+            .iter()
+            .map(|dir| Directory::flatten(dir))
+            .flatten()
+            .collect();
+
+        children.push(Rc::clone(directory));
+
+        return children;
+    }
+
+    fn get_smallest_valid_dir(directory: &Rc<RefCell<Directory>>, file_system_size: usize, min_size: usize) -> usize {
+        let free_space= file_system_size - directory.borrow().get_size();
+
+        Directory::flatten(directory)
+            .iter()
+            .map(|dir| dir.borrow().get_size())
+            .filter(|size| *size >= min_size - free_space)
+            .min()
+            .unwrap()
+    }
+
 }
 
 fn parse_file_tree(str: &str) -> Rc<RefCell<Directory>> {
@@ -42,13 +68,10 @@ fn parse_file_tree(str: &str) -> Rc<RefCell<Directory>> {
     for directory_string in str.split("$ "){
         let cd_regex = Regex::new(r"cd (?P<name>.+)\n").unwrap();
         let ls_regex= Regex::new(r"(?s)ls\n(?P<dirlist>.*)").unwrap();
-        let dir_regex= Regex::new(r"dir (?P<name>.+)").unwrap();
 
         match cd_regex.captures(directory_string) {
             Some(capture) => match capture.name("name").map(|name| name.as_str()) {
-                None => {
-                    let a = 1;
-                }
+                None => {}
                 Some("..") => {
                     current_dir = get_parent_dir(current_dir);
                 }
@@ -64,9 +87,7 @@ fn parse_file_tree(str: &str) -> Rc<RefCell<Directory>> {
                     Some(capture) => {
                         parse_dir_content( Rc::clone(&current_dir), capture.name("dirlist").unwrap().as_str());
                     }
-                    None => {
-                        let a = "a";
-                    }
+                    None => {}
                 }
             }
         }
@@ -82,8 +103,6 @@ fn get_parent_dir(current_dir: Rc<RefCell<Directory>>) -> Rc<RefCell<Directory>>
 fn get_child_dir(current_dir: Rc<RefCell<Directory>>, name: &str) -> Rc<RefCell<Directory>> {
     let a: RefMut<Directory> = current_dir.borrow_mut();
     Rc::clone(a.directories.iter().find(|dir| dir.borrow_mut().name == name).unwrap())
-    //current_dir
-   // Rc::clone(current_dir..directories.iter().find(|dir| dir.borrow().name == name).unwrap())
 }
 
 fn parse_dir_content(directory: Rc<RefCell<Directory>>, str: &str) {
@@ -100,13 +119,13 @@ fn parse_dir_content(directory: Rc<RefCell<Directory>>, str: &str) {
         })))
         .collect();
 
-    let regex_files = Regex::new(r"(?P<size>\d+) (?P<filename>.+)\n").unwrap();
+    let regex_files = Regex::new(r"(?P<size>\d+) (?P<filename>.*)\n").unwrap();
 
     let files = regex_files
         .captures_iter(str)
         .map(|mat| File {
-            name: mat.name("filename").unwrap().as_str().parse().unwrap(),
-            size: mat.name("size").unwrap().as_str().parse::<usize>().unwrap()
+                name: mat.name("filename").unwrap().as_str().parse().unwrap(),
+                size: mat.name("size").unwrap().as_str().parse::<usize>().unwrap()
         });
 
     let mut dir = directory.borrow_mut();
@@ -127,5 +146,15 @@ mod tests {
     #[test]
     fn large_test() {
         assert_eq!(parse_file_tree(fs::read_to_string("src/day7/test_large.txt").unwrap().as_mut_str()).borrow().get_sizes_under(100000), 1391690);
+    }
+
+    #[test]
+    fn simple_test_2() {
+        assert_eq!(Directory::get_smallest_valid_dir(&parse_file_tree(fs::read_to_string("src/day7/test_simple.txt").unwrap().as_mut_str()), 70000000, 30000000), 24933642);
+    }
+
+    #[test]
+    fn large_test_2() {
+        assert_eq!(Directory::get_smallest_valid_dir(&parse_file_tree(fs::read_to_string("src/day7/test_large.txt").unwrap().as_mut_str()), 70000000, 30000000), 5469168);
     }
 }
